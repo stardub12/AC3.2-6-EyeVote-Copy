0d24681b4d0c70c2c233274ad7717a5a4deafa32
Edward Anchundia <edwardanchundia@MacBook-Pro.local>
2017-02-06 17:54:36 -0500

    view controllers

---
+++ EyeVote/EyeVote/GalleryTableViewCell.swift
@@ -0,0 +1,45 @@
+//
+//  TableViewCell.swift
+//  EyeVote
+//
+//  Created by Edward Anchundia on 2/6/17.
+//  Copyright © 2017 Edward Anchundia. All rights reserved.
+//
+
+import UIKit
+
+class GalleryTableViewCell: UITableViewCell {
+    
+    var categoryLabel = UILabel()
+    var categoryImage = UIImageView()
+
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        categoryImage.backgroundColor = UIColor.gray
+        
+        self.contentView.addSubview(categoryImage)
+        self.contentView.addSubview(categoryLabel)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        categoryImage.snp.makeConstraints { (view) in
+            view.width.equalTo(contentView.snp.width)
+            view.height.equalTo(contentView.snp.height)
+            view.center.equalTo(contentView.center)
+        }
+        
+        categoryLabel.snp.makeConstraints { (view) in
+            view.width.equalTo(100)
+            view.height.equalTo(50)
+            view.center.equalTo(categoryImage.snp.center)
+        }
+    }
+
+}
